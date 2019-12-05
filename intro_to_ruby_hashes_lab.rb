c11a64485e2681b14ee2d52c66e2227d9eea8b99
Riaan Taveras <riaantaveras@gmail.com>
2019-12-05 00:10:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,11 +17,11 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  {:key => value}
+  {key => value}
 end
 
 def read_from_hash(hash, key)
-  hash[:key]
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
