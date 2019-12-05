d5e80c0ff8eae7a1bbbe8d63f79dac11a02c807b
Riaan Taveras <riaantaveras@gmail.com>
2019-12-05 00:31:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,9 +25,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash[key]
-    hash[key] = hash[key] + 1
-  else
+  if
     hash = {key => 1}
+  else
+    hash[key]
+    hash[key] = hash[key] + 1
   end
 end
